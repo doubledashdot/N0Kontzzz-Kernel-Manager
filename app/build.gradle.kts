f0cb04ee03f3389e72c41d7acb1c136b4b1cbf10
@skye <sonicgames772@gmail.com>
2026-04-28 22:13:52 -0300

    add github ci pipeline
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ configure <com.android.build.api.dsl.ApplicationExtension> {
         targetCompatibility = JavaVersion.VERSION_17
     }
     lint {
+        disable.add("NonObservableLocale")
         disable.add("NullSafeMutableLiveData")
     }
     buildFeatures { compose = true }
